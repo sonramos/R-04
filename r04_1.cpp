3f14d495cca2f0f829afd1f9ed9a01165b85d9c5
sonramos <jayradpro@gmail.com>
2017-03-02 09:40:00 -0300

    atividade 1

--- r04_1.cpp
+++ r04_1.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-using namespace std;
 
 int main(){
+	int x, a = 0;
 	
-
+	std::cout << "Iniciando programa: \"Qual o maior numero?!\"\n\nPara desligar agora, insira 0.\nInsira um numero: ";
+	std::cin >> x;
+	while(x != 0){
+		if (x > a){
+			std::cout << "Maior numero: " << x << "\n";
+			a = x;
+			}
+			else
+			std::cout << "Maior numero: " << a << "\n";
+		std::cout << "\nInsira um numero: ";
+		std::cin >> x;
+	} 		
 }
